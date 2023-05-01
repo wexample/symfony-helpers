9592c0d189d284b1b6cb61ecc86a5f4ef57a5f02
weeger <romain.weeger@gmail.com>
2023-05-01 20:19:09 +0200

    Type

---
+++ src/Helper/TypesHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+class TypesHelper {
+    const TARRAY = 'array';
+    const SIMPLE_ARRAY = 'simple_array';
+    const JSON_ARRAY = 'json_array';
+    const BIGINT = 'bigint';
+    const BOOLEAN = 'boolean';
+    const DATETIME = 'datetime';
+    const DATETIMETZ = 'datetimetz';
+    const DATE = 'date';
+    const TIME = 'time';
+    const DECIMAL = 'decimal';
+    const INTEGER = 'integer';
+    const OBJECT = 'object';
+    const SMALLINT = 'smallint';
+    const STRING = 'string';
+    const TEXT = 'text';
+    const BLOB = 'blob';
+    const FLOAT = 'float';
+    const GUID = 'guid';
+}
