c0f652f278e65e70031120b66fc688ea1e03f6af
weeger <romain.weeger@gmail.com>
2023-05-24 10:29:37 +0200

    Test routing configuration

--- src/Helper/VariableHelper.php
+++ src/Helper/VariableHelper.php
@@ -5,8 +5,10 @@ namespace Wexample\SymfonyHelpers\Helper;
 class VariableHelper
 {
     public const DATA = 'data';
+    public const ERROR = 'error';
     public const ID = 'id';
     public const LENGTH = 'length';
     public const PAGE = 'page';
+    public const SUCCESS = 'success';
     public const YEAR = 'year';
 }
