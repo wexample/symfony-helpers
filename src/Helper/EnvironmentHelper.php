d888fd8f7bfb1058dc3f991818bac59fdb5f9c86
weeger <romain.weeger@gmail.com>
2023-06-12 11:14:56 +0200

    Env helper

---
+++ src/Helper/EnvironmentHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Wexample\SymfonyHelpers\Helper;
+
+class EnvironmentHelper
+{
+    final public const DEV = 'dev';
+
+    final public const LOCAL = 'local';
+
+    final public const PROD = 'prod';
+
+    final public const STAGING = 'staging';
+
+    final public const TEST = 'test';
+
+    final public const LIST_UNREACHABLE = [
+        self::LOCAL,
+        self::TEST,
+    ];
+
+    final public const LIST_LOW_SECURITY = [
+        self::DEV,
+        self::LOCAL,
+        self::TEST,
+    ];
+}
