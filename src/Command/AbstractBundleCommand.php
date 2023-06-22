0ed859f88846db13059e68b7850a20789fdbd269
weeger <romain.weeger@gmail.com>
2023-06-22 15:19:26 +0200

    Commands and dependencies

--- src/Command/AbstractBundleCommand.php
+++ src/Command/AbstractBundleCommand.php
@@ -3,13 +3,17 @@
 namespace Wexample\SymfonyHelpers\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Wexample\SymfonyHelpers\Helper\ClassHelper;
 use Wexample\SymfonyHelpers\Helper\TextHelper;
 
 abstract class AbstractBundleCommand extends Command
 {
-    public function __construct(string $name = null)
-    {
+    public function __construct(
+        protected KernelInterface $kernel,
+        string $name = null,
+    ) {
         parent::__construct($name ?: $this->buildDefaultName());
     }
 
@@ -18,7 +22,7 @@ abstract class AbstractBundleCommand extends Command
         return TextHelper::toKebab(
                 TextHelper::removePrefix(
                     TextHelper::removeSuffix(
-                        ClassHelper::getShortName(static::getBundle()),
+                        ClassHelper::getShortName(static::getBundleClassName()),
                         'Bundle'
                     ),
                     'Wexample'
@@ -33,5 +37,19 @@ abstract class AbstractBundleCommand extends Command
             );
     }
 
-    abstract public static function getBundle(): string;
+    abstract public static function getBundleClassName(): string;
+
+    protected function getBundleRootPath():string
+    {
+        return realpath($this->getBundle()->getPath() . '/../') . '/';
+    }
+
+    protected function getBundle(): BundleInterface
+    {
+        return $this->kernel->getBundle(
+            ClassHelper::getShortName(
+                $this::getBundleClassName()
+            )
+        );
+    }
 }
---
+++ src/Command/AbstractCheckNodeInstallCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Wexample\SymfonyHelpers\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Wexample\SymfonyHelpers\Helper\JsonHelper;
+
+abstract class AbstractCheckNodeInstallCommand extends AbstractBundleCommand
+{
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Check dependencies installation');
+    }
+
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $io = new SymfonyStyle($input, $output);
+
+        $packageJsonPath = $this->kernel->getProjectDir().'/package.json';
+
+        if (!file_exists($packageJsonPath)) {
+            $io->error('No package.json file found.');
+
+            return Command::FAILURE;
+        }
+
+        $packageJsonContent = file_get_contents($packageJsonPath);
+        $packageJsonData = json_decode($packageJsonContent, true);
+        $dependencyFile = $this->getBundleRootPath().'package.dependencies.json';
+
+        if (!is_file($dependencyFile)) {
+            $io->error('Missing file : '.$dependencyFile);
+            return Command::FAILURE;
+        }
+
+        $neededDependencies = JsonHelper::read(
+            $dependencyFile,
+        );
+
+        $missingDependencies = [];
+
+        foreach ($neededDependencies->dependencies as $dependency) {
+            if (!isset($packageJsonData['dependencies'][$dependency]) && !isset($packageJsonData['devDependencies'][$dependency])) {
+                $missingDependencies[] = $dependency;
+            }
+        }
+
+        if (!empty($missingDependencies)) {
+            $missingDependenciesString = implode(' ', $missingDependencies);
+            $io->error("Missing node modules: '{$missingDependenciesString}'. Run `npm install {$missingDependenciesString}` or `yarn add {$missingDependenciesString}`.");
+
+            return Command::FAILURE;
+        }
+
+        $io->success('All dependencies are installed.');
+
+        return Command::SUCCESS;
+    }
+}
