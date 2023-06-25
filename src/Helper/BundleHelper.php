3e772d45924e76629cd672e90ba28f21ce0c425c
weeger <romain.weeger@gmail.com>
2023-06-25 17:54:44 +0200

    Move var

--- src/Helper/BundleHelper.php
+++ src/Helper/BundleHelper.php
@@ -9,6 +9,7 @@ class BundleHelper
     final public const BUNDLE_PATH_RESOURCES = 'Resources'.FileHelper::FOLDER_SEPARATOR;
     final public const BUNDLE_PATH_TEMPLATES = self::BUNDLE_PATH_RESOURCES.self::DIR_TEMPLATES;
     final public const CLASS_PATH_PREFIX = ClassHelper::NAMESPACE_SEPARATOR.'App'.ClassHelper::NAMESPACE_SEPARATOR;
+    final public const COMPOSER_JSON_FILE_NAME = 'composer.json';
     final public const DIR_SRC = self::FOLDER_SRC.FileHelper::FOLDER_SEPARATOR;
     final public const DIR_TEMPLATES = 'templates'.FileHelper::FOLDER_SEPARATOR;
     final public const FOLDER_SRC = 'src';
