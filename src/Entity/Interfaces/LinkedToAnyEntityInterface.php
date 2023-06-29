ee192e658d2e99bb27e8f3130d49eae93e07b0d1
weeger <romain.weeger@gmail.com>
2023-06-29 12:44:04 +0200

    Move helper

---
+++ src/Entity/Interfaces/LinkedToAnyEntityInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wexample\SymfonyHelpers\Entity\Interfaces;
+
+interface LinkedToAnyEntityInterface
+{
+    /**
+     * string.
+     */
+    public const PROPERTY_NAME_ENTITY_TYPE = 'entityType';
+
+    /**
+     * string.
+     */
+    public const PROPERTY_NAME_ENTITY_ID = 'entityId';
+}
