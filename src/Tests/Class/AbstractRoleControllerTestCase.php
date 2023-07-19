aa3eff394e90655bf688594e3557175b06b92b1c
weeger <romain.weeger@gmail.com>
2023-07-19 09:07:46 +0200

    Add exception handling in AbstractRoleControllerTestCase
    This commit introduces exception handling when guessing controller class name from test controller class name in AbstractRoleControllerTestCase. We now check whether the controller class exists or not to prevent potential errors. If the class does not exist, it throws an exception.

--- src/Tests/Class/AbstractRoleControllerTestCase.php
+++ src/Tests/Class/AbstractRoleControllerTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Wexample\SymfonyHelpers\Tests\Class;
 
+use Exception;
 use Wexample\SymfonyHelpers\Helper\ClassHelper;
 use Wexample\SymfonyHelpers\Helper\TextHelper;
 use Wexample\SymfonyHelpers\Service\Syntax\ControllerSyntaxService;
@@ -22,10 +23,13 @@ abstract class AbstractRoleControllerTestCase extends AbstractRoleTestCase
      * Guess controller class name from test controller class name.
      *
      * @param string|null $testControllerClass
+     * @param bool        $checkExists
      * @return string
+     * @throws Exception
      */
     public static function buildControllerClassPath(
-        string $testControllerClass = null
+        string $testControllerClass = null,
+        bool $checkExists = true
     ): string {
         $testControllerClass = $testControllerClass ?: static::class;
 
@@ -48,8 +52,15 @@ abstract class AbstractRoleControllerTestCase extends AbstractRoleTestCase
         $chunks = array_splice($chunks, $removeChunksLength);
 
         array_unshift($chunks, ClassHelper::CLASS_PATH_PART_APP);
+        $controllerClass = ClassHelper::join($chunks, true);
 
-        return ClassHelper::join($chunks, true);
+        if ($checkExists) {
+            if (!class_exists($controllerClass)) {
+                throw new Exception('Unable to find controller class from ' . $testControllerClass . ', tried ' . $controllerClass);
+            }
+        }
+
+        return $controllerClass;
     }
 
     protected function setUp(): void
