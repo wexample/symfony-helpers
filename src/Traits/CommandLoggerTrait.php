c7c0207d0da35783cbf50d9e6e5e101016193f13
weeger <romain.weeger@gmail.com>
2023-09-23 13:07:38 +0200

    First commit

---
+++ src/Traits/CommandLoggerTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Wexample\SymfonyHelpers\Traits;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Wexample\SymfonyHelpers\Helper\TextHelper;
+
+trait CommandLoggerTrait
+{
+    use ConsoleLoggerTrait;
+
+    public ?OutputInterface $output = null;
+
+    public function log(
+        string $message,
+        string $color = TextHelper::ASCII_COLOR_WHITE,
+        int $indent = null
+    ): void {
+        $message = $this->formatLogMessage($message, $color, $indent);
+
+        if ($this->output) {
+            $this->output->writeln($message);
+        } else {
+            echo PHP_EOL.$message;
+        }
+    }
+}
