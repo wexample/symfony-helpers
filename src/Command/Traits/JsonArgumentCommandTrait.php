c533028709b4538eb57eb22b8820251ce06c8a70
weeger <romain.weeger@gmail.com>
2025-03-17 15:12:03 +0100

    Export

--- src/Command/Traits/JsonArgumentCommandTrait.php
+++ src/Command/Traits/JsonArgumentCommandTrait.php
@@ -48,4 +48,45 @@ trait JsonArgumentCommandTrait
 
         return $data;
     }
+    
+    /**
+     * Write data to a JSON file
+     * 
+     * @param InputInterface $input
+     * @param array $data The data to write to the file
+     * @param int $options JSON encoding options
+     * @return string The full file path where the data was written
+     * @throws InvalidArgumentException If the file cannot be written
+     */
+    protected function writeJsonFile(InputInterface $input, array $data, int $options = 0): string
+    {
+        $filePath = $input->getArgument('file');
+        $fullFilePath = $this->getFullFilePath($filePath);
+        
+        // Create directory if it doesn't exist
+        $directory = dirname($fullFilePath);
+        if (!is_dir($directory)) {
+            if (!mkdir($directory, 0777, true) && !is_dir($directory)) {
+                throw new InvalidArgumentException(sprintf('Directory "%s" could not be created', $directory));
+            }
+        }
+        
+        // Default options for better readability
+        if ($options === 0) {
+            $options = JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE;
+        }
+        
+        // Encode data to JSON
+        $jsonContent = json_encode($data, $options);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new InvalidArgumentException(sprintf('Failed to encode data to JSON: %s', json_last_error_msg()));
+        }
+        
+        // Write to file
+        if (file_put_contents($fullFilePath, $jsonContent) === false) {
+            throw new InvalidArgumentException(sprintf('Failed to write to file %s', $fullFilePath));
+        }
+        
+        return $fullFilePath;
+    }
 }
