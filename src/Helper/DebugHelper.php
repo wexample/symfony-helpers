e4c46c8ef6b95563089cb76a24313282c2df0c97
weeger <romain.weeger@gmail.com>
2025-04-09 08:48:58 +0200

    Helper

--- src/Helper/DebugHelper.php
+++ src/Helper/DebugHelper.php
@@ -89,4 +89,65 @@ class DebugHelper
 
         return (string) $var;
     }
+
+    /**
+     * Format an exception trace array into a readable string
+     *
+     * @param array $trace The trace array from Exception::getTrace()
+     * @param bool $ignoreArgs If true, function arguments will not be included in the output
+     * @param int $maxFrames Maximum number of frames to display (0 = all)
+     * @return string Formatted trace string
+     */
+    public static function formatTrace(array $trace, bool $ignoreArgs = false, int $maxFrames = 0): string
+    {
+        $result = "";
+        $frameCount = count($trace);
+        $limit = $maxFrames > 0 ? min($frameCount, $maxFrames) : $frameCount;
+
+        for ($i = 0; $i < $limit; $i++) {
+            $frame = $trace[$i];
+            $file = $frame['file'] ?? '(unknown file)';
+            $line = $frame['line'] ?? '(unknown line)';
+            $class = $frame['class'] ?? '';
+            $type = $frame['type'] ?? '';
+            $function = $frame['function'] ?? '(unknown function)';
+
+            $result .= sprintf("#%d %s:%d %s%s%s(", $i, $file, $line, $class, $type, $function);
+
+            // Add function arguments if requested
+            if (!$ignoreArgs && isset($frame['args']) && is_array($frame['args'])) {
+                $argsStr = [];
+                foreach ($frame['args'] as $arg) {
+                    if (is_object($arg)) {
+                        $argsStr[] = get_class($arg);
+                    } elseif (is_array($arg)) {
+                        $argsStr[] = 'Array(' . count($arg) . ')';
+                    } elseif (is_string($arg)) {
+                        $argStr = strlen($arg) > 50 ? substr($arg, 0, 47) . '...' : $arg;
+                        $argsStr[] = "'" . addslashes($argStr) . "'";
+                    } elseif (is_null($arg)) {
+                        $argsStr[] = 'NULL';
+                    } elseif (is_bool($arg)) {
+                        $argsStr[] = $arg ? 'true' : 'false';
+                    } elseif (is_resource($arg)) {
+                        $argsStr[] = get_resource_type($arg);
+                    } else {
+                        $argsStr[] = (string)$arg;
+                    }
+                }
+                $result .= implode(', ', $argsStr);
+            } else if (!$ignoreArgs) {
+                $result .= '...';
+            }
+
+            $result .= ")\n";
+        }
+
+        // Add a note if we truncated the trace
+        if ($maxFrames > 0 && $frameCount > $maxFrames) {
+            $result .= sprintf("... %d more frames omitted ...\n", $frameCount - $maxFrames);
+        }
+
+        return $result;
+    }
 }
