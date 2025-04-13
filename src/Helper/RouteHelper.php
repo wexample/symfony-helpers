d13d88ba855482d47a2b28f532388e0ad3c08736
weeger <romain.weeger@gmail.com>
2025-04-13 12:42:34 +0200

    Auto routing Template route based loder

--- src/Helper/RouteHelper.php
+++ src/Helper/RouteHelper.php
@@ -2,6 +2,7 @@
 
 namespace Wexample\SymfonyHelpers\Helper;
 
+use Symfony\Component\Routing\RouteCollection;
 use Wexample\Helpers\Helper\ClassHelper;
 use Wexample\Helpers\Helper\TextHelper;
 use Wexample\SymfonyHelpers\Controller\AbstractController;
@@ -10,13 +11,39 @@ class RouteHelper
 {
     public static function buildRoutePrefixFromControllerClass(
         AbstractController|string $controllerClass
-    ): string {
+    ): string
+    {
         return TextHelper::toSnake(
                 AbstractController::removeSuffix(
                     ClassHelper::getShortName(
                         $controllerClass
                     )
                 )
-            ).'_';
+            ) . '_';
+    }
+
+    public static function filterRouteByController(
+        RouteCollection $allRoutes,
+        string $controllerClass
+    ): array
+    {
+        $routes = [];
+
+        foreach ($allRoutes as $name => $route) {
+            $defaults = $route->getDefaults();
+
+            if (isset($defaults['_controller'])) {
+                // Extract the controller class name from _controller
+                // Typical format: 'App\Controller\MyController::myMethod'
+                $parts = explode('::', $defaults['_controller']);
+                $routeControllerClass = $parts[0];
+
+                if ($routeControllerClass === $controllerClass) {
+                    $routes[$name] = $route;
+                }
+            }
+        }
+
+        return $routes;
     }
 }
