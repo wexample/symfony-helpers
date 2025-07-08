ed107017b59a7719de7f5fdc774956a6523460a7
weeger <romain.weeger@gmail.com>
2025-07-08 17:20:15 +0200

    Card position

--- src/Entity/Traits/HasPositionsXYTrait.php
+++ src/Entity/Traits/HasPositionsXYTrait.php
@@ -6,4 +6,13 @@ trait HasPositionsXYTrait
 {
     use HasPositionXTrait;
     use HasPositionYTrait;
+
+    public function setPositionXY(
+        int $positionX,
+        int $positionY
+    )
+    {
+        $this->setPositionX($positionX);
+        $this->setPositionY($positionY);
+    }
 }
