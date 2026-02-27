764ac468b53fe1345c14a7ce7b57bcef5f869b95
weeger <romain.weeger@gmail.com>
2026-02-27 16:20:13 +0100

    Rectify

--- src/Service/RectifyService.php
+++ src/Service/RectifyService.php
@@ -4,6 +4,7 @@ namespace Wexample\SymfonyHelpers\Service;
 
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
+use ReflectionClass;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Wexample\SymfonyHelpers\Attribute\RectifiableEntity;
 use Wexample\SymfonyHelpers\Entity\AbstractEntity;
@@ -23,10 +24,20 @@ class RectifyService
         $violations = [];
 
         foreach ($this->findEntityClasses() as $entityClass) {
-            $hasRectifiableEntityAttribute = (new \ReflectionClass($entityClass))
+            $reflectionClass = new ReflectionClass($entityClass);
+            $hasRectifiableEntityAttribute = $reflectionClass
                 ->getAttributes(RectifiableEntity::class) !== [];
 
             if (! $hasRectifiableEntityAttribute) {
+                continue;
+            }
+
+            $parentClass = $reflectionClass->getParentClass();
+
+            if ($parentClass === false) {
+                $this->addAbstractEntityExtends($reflectionClass);
+                $this->removeDefaultIdBlockIfItIsTheOnlyClassContent($reflectionClass);
+
                 continue;
             }
 
@@ -37,6 +48,8 @@ class RectifyService
                     AbstractEntity::class
                 );
             }
+
+            $this->removeDefaultIdBlockIfItIsTheOnlyClassContent($reflectionClass);
         }
 
         return $violations;
@@ -75,5 +88,70 @@ class RectifyService
         }
 
         return $classes;
+    }
+
+    private function addAbstractEntityExtends(
+        ReflectionClass $reflectionClass
+    ): void {
+        $filePath = $reflectionClass->getFileName();
+        $content = file_get_contents($filePath);
+
+        if (! str_contains($content, 'use Wexample\\SymfonyHelpers\\Entity\\AbstractEntity;')) {
+            $content = preg_replace(
+                '/^namespace\s+[^;]+;\n/m',
+                "$0\nuse Wexample\\SymfonyHelpers\\Entity\\AbstractEntity;\n",
+                $content,
+                1
+            );
+        }
+
+        $className = $reflectionClass->getShortName();
+        $content = preg_replace(
+            '/\bclass\s+'.preg_quote($className, '/').'\b(?!\s+extends)(\s*(?:implements\s+[^{]+)?)\s*\{/m',
+            'class '.$className.' extends AbstractEntity$1 {',
+            $content,
+            1
+        );
+
+        file_put_contents($filePath, $content);
+    }
+
+    private function removeDefaultIdBlockIfItIsTheOnlyClassContent(
+        ReflectionClass $reflectionClass
+    ): void {
+        $filePath = $reflectionClass->getFileName();
+        $content = file_get_contents($filePath);
+        $className = $reflectionClass->getShortName();
+
+        $pattern = '/(class\s+'.preg_quote($className, '/').'\b[^{]*\{)(?<body>[\s\S]*?)(^\})/m';
+        if (! preg_match($pattern, $content, $matches)) {
+            return;
+        }
+
+        $normalizedBody = preg_replace('/\s+/', '', $matches['body']);
+        $normalizedSymfonyDefaultIdBlock = preg_replace('/\s+/', '', <<<'PHP'
+#[ORM\Id]
+#[ORM\GeneratedValue]
+#[ORM\Column]
+private ?int $id = null;
+
+public function getId(): ?int
+{
+    return $this->id;
+}
+PHP);
+
+        if ($normalizedBody !== $normalizedSymfonyDefaultIdBlock) {
+            return;
+        }
+
+        $content = preg_replace(
+            $pattern,
+            '$1'."\n".'$3',
+            $content,
+            1
+        );
+
+        file_put_contents($filePath, $content);
     }
 }
