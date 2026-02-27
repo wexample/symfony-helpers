70b4384d78a0b0d4e27f89d12b217f3cd7294aba
weeger <romain.weeger@gmail.com>
2026-02-27 16:27:31 +0100

    Rectify

--- src/Service/Rectify/Rule/RepositoryRule.php
+++ src/Service/Rectify/Rule/RepositoryRule.php
@@ -28,6 +28,18 @@ class RepositoryRule extends AbstractRectifyRule
         }
 
         if (! is_subclass_of($repositoryClass, AbstractRepository::class, true)) {
+            $repositoryReflection = new ReflectionClass($repositoryClass);
+            $repositoryParentClass = $repositoryReflection->getParentClass();
+
+            if ($repositoryParentClass !== false && $repositoryParentClass->getName() === 'Doctrine\\Bundle\\DoctrineBundle\\Repository\\ServiceEntityRepository') {
+                $this->rectifyInitialSymfonyRepository(
+                    $entityReflection,
+                    $repositoryReflection
+                );
+
+                return [];
+            }
+
             return [
                 sprintf(
                     '%s repository %s must extend %s.',
@@ -39,6 +51,113 @@ class RepositoryRule extends AbstractRectifyRule
         }
 
         return [];
+    }
+
+    private function rectifyInitialSymfonyRepository(
+        ReflectionClass $entityReflection,
+        ReflectionClass $repositoryReflection
+    ): void {
+        $filePath = $repositoryReflection->getFileName();
+        $content = file_get_contents($filePath);
+
+        $content = str_replace(
+            "use Doctrine\\Bundle\\DoctrineBundle\\Repository\\ServiceEntityRepository;\n",
+            '',
+            $content
+        );
+        $content = str_replace(
+            "use Doctrine\\Persistence\\ManagerRegistry;\n",
+            '',
+            $content
+        );
+
+        if (! str_contains($content, 'use Wexample\\SymfonyHelpers\\Repository\\AbstractRepository;')) {
+            $content = preg_replace(
+                '/^namespace\s+[^;]+;\n/m',
+                "$0\nuse Wexample\\SymfonyHelpers\\Repository\\AbstractRepository;\n",
+                $content,
+                1
+            );
+        }
+
+        $repositoryShortName = $repositoryReflection->getShortName();
+        $content = preg_replace(
+            '/\bclass\s+'.preg_quote($repositoryShortName, '/').'\s+extends\s+[^\s{]+/m',
+            'class '.$repositoryShortName.' extends AbstractRepository',
+            $content,
+            1
+        );
+
+        $content = preg_replace(
+            '/\n\/\*\*\s*\n\s*\*\s*@extends ServiceEntityRepository<[^>]+>\s*\n\s*\*\/\n/m',
+            "\n",
+            $content,
+            1
+        );
+
+        $content = $this->removeInitialSymfonyRepositoryBodyIfNeeded(
+            $content,
+            $repositoryReflection->getShortName(),
+            $entityReflection->getShortName()
+        );
+
+        file_put_contents($filePath, $content);
+    }
+
+    private function removeInitialSymfonyRepositoryBodyIfNeeded(
+        string $content,
+        string $repositoryShortName,
+        string $entityShortName
+    ): string {
+        $alias = strtolower($entityShortName[0]);
+        $pattern = '/(class\s+'.preg_quote($repositoryShortName, '/').'\b[^{]*\{)(?<body>[\s\S]*?)(^\})/m';
+        if (! preg_match($pattern, $content, $matches)) {
+            return $content;
+        }
+
+        $normalizedBody = preg_replace('/\s+/', '', $matches['body']);
+        $normalizedSymfonyDefaultBody = preg_replace('/\s+/', '', <<<PHP
+public function __construct(ManagerRegistry \$registry)
+{
+    parent::__construct(\$registry, {$entityShortName}::class);
+}
+
+//    /**
+//     * @return {$entityShortName}[] Returns an array of {$entityShortName} objects
+//     */
+//    public function findByExampleField(\$value): array
+//    {
+//        return \$this->createQueryBuilder('{$alias}')
+//            ->andWhere('{$alias}.exampleField = :val')
+//            ->setParameter('val', \$value)
+//            ->orderBy('{$alias}.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField(\$value): ?{$entityShortName}
+//    {
+//        return \$this->createQueryBuilder('{$alias}')
+//            ->andWhere('{$alias}.exampleField = :val')
+//            ->setParameter('val', \$value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+PHP);
+
+        if ($normalizedBody !== $normalizedSymfonyDefaultBody) {
+            return $content;
+        }
+
+        return preg_replace(
+            $pattern,
+            '$1'."\n".'$3',
+            $content,
+            1
+        );
     }
 
     private function resolveRepositoryClass(
