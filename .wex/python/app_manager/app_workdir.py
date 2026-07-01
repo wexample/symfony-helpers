29520c81795015f8b2976129d68bbceab97045fb
weeger <romain.weeger@gmail.com>
2026-07-01 16:53:41 +0200

    Various fix

--- .wex/python/app_manager/app_workdir.py
+++ .wex/python/app_manager/app_workdir.py
@@ -6,7 +6,7 @@ from wexample_wex_addon_dev_php.workdir.php_package_workdir import PhpPackageWor
 
 class AppWorkdir(PhpPackageWorkdir):
     def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
-        from wexample_helpers.helpers.string import string_to_kebab_case
+        from wexample_helpers.helper.string import string_to_kebab_case
 
         raw_value = super().prepare_value(raw_value=raw_value)
 
