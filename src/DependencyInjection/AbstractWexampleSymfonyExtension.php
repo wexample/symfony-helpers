8bde88488e021d799fb68875a294394fc8191167
weeger <romain.weeger@gmail.com>
2026-07-28 13:15:27 +0200

    Cleanup

--- src/DependencyInjection/AbstractWexampleSymfonyExtension.php
+++ src/DependencyInjection/AbstractWexampleSymfonyExtension.php
@@ -5,10 +5,42 @@ namespace Wexample\SymfonyHelpers\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Wexample\Helpers\Helper\ClassHelper;
 
-abstract class AbstractWexampleSymfonyExtension extends Extension
+abstract class AbstractWexampleSymfonyExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        $entityDir = __DIR__ . '/../Entity';
+
+        if (!is_dir($entityDir)) {
+            return;
+        }
+
+        $alias = str_replace('Extension', '', ClassHelper::getShortName(static::class));
+        $entityNamespace = str_replace(
+            '\\DependencyInjection',
+            '\\Entity',
+            substr(static::class, 0, strrpos(static::class, '\\'))
+        );
+
+        $container->prependExtensionConfig('doctrine', [
+            'orm' => [
+                'mappings' => [
+                    $alias => [
+                        'type'      => 'attribute',
+                        'dir'       => $entityDir,
+                        'prefix'    => $entityNamespace,
+                        'alias'     => $alias,
+                        'is_bundle' => false,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
     protected function loadConfig(
         $callingDir,
         ContainerBuilder $container,
